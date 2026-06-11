04e7b811f14f43b3c28be4488d2b3a9a4c3cc715
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-06-11 14:00:39 -0300

    style: melhorando o estilo do menu de escolha de cores e wallpapers

--- src/hooks/useBackground.tsx
+++ src/hooks/useBackground.tsx
@@ -3,6 +3,12 @@ import Wallpaper1 from "../assets/wallpaper-01.jpg";
 import Wallpaper2 from "../assets/wallpaper-02.jpg";
 import Wallpaper3 from "../assets/wallpaper-03.jpg";
 import Wallpaper4 from "../assets/wallpaper-04.jpg";
+import Wallpaper5 from "../assets/wallpaper-05.jpg";
+import Wallpaper6 from "../assets/wallpaper-06.jpg";
+import Wallpaper7 from "../assets/wallpaper-07.jpg";
+import Wallpaper8 from "../assets/wallpaper-08.jpg";
+import Wallpaper9 from "../assets/wallpaper-09.jpg";
+import Wallpaper10 from "../assets/wallpaper-010.jpg";
 
 export default function useBackground() {
   const listaMudarFundo = [
@@ -18,6 +24,24 @@ export default function useBackground() {
     {
       img: Wallpaper4,
     },
+    {
+      img: Wallpaper5,
+    },
+    {
+      img: Wallpaper6,
+    },
+    {
+      img: Wallpaper7,
+    },
+    {
+      img: Wallpaper8,
+    },
+    {
+      img: Wallpaper9,
+    },
+    {
+      img: Wallpaper10,
+    }
   ];
   
   const [showFormMudarFundo, setShowFormMudarFundo] = useState(false);
--- src/page/PaginaQuadro/components/FormMudarFundo/index.tsx
+++ src/page/PaginaQuadro/components/FormMudarFundo/index.tsx
@@ -21,38 +21,42 @@ export default function FormMudarFundo({
   const { listaMudarFundo } = useBackground();
 
   return (
-    <div className="wallpapers-list-container">
-      <div className="colors">
-        <div>
-          <p>Escolha as cores que terá na colunas</p>
-          <input
-            type="color"
-            value={corAtual}
-            onChange={(e) => onColorChange(e.target.value)}
-          />
+    <div className="modal-overlay">
+      <div className="wallpapers-list-container">
+        <div className="scroll-area">
+          <div className="colors">
+            <div>
+              <p>Cor das Colunas: </p>
+              <input
+                type="color"
+                value={corAtual}
+                onChange={(e) => onColorChange(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <p>Cor das fontes: </p>
+              <input
+                type="color"
+                value={corFonte}
+                onChange={(e) => onColorFontChange(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="items">
+            {listaMudarFundo.map((el, index) => (
+              <button key={index} onClick={() => onSelect(el.img)}>
+                <img src={el.img} className="imgs" />
+              </button>
+            ))}
+          </div>
         </div>
 
-        <div>
-          <p>Escolha a cor que terá na fonte</p>
-          <input
-            type="color"
-            value={corFonte}
-            onChange={(e) => onColorFontChange(e.target.value)}
-          />
-        </div>
+        <button onClick={onCancel} id="btn_sair">
+          Sair
+        </button>
       </div>
-      <div className="items">
-        {listaMudarFundo.map((el, index) => {
-          return (
-            <button key={index} onClick={() => onSelect(el.img)}>
-              <img src={el.img} className="imgs" />
-            </button>
-          );
-        })}
-      </div>
-      <button onClick={onCancel} id="btn_sair">
-        Sair
-      </button>
     </div>
   );
 }
