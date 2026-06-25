cc5e8797a728a508ff1424110410eea90287a07c
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-06-25 12:24:22 -0300

    fix: corrigindo problemas de roteamento com browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import PaginaQuadro from "./page/PaginaQuadro";
 import Header from "./layout/Header";
 export default function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quadro/:id" element={<PaginaQuadro />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/quadro/:id" element={<PaginaQuadro />} />
+			</Routes>
+		</>
+	);
 }
